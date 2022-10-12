8262d2b5610c6f057204ba14056dff4c9126ba87
JacobLinCool <jacoblincool@gmail.com>
2022-10-12 18:19:07 +0800

    fix: random name string end char

--- src/utils.c
+++ src/utils.c
@@ -1,9 +1,10 @@
 #include "utils.h"
 
 char* random_name(u32 length) {
-	char* str = malloc(sizeof(char) * length);
+	char* str = malloc(sizeof(char) * (length + 1));
 	for (u32 i = 0; i < length; ++i) {
 		str[i] = (char)(rand() % 26 + 97);
 	}
+	str[length] = '\0';
 	return str;
 }
